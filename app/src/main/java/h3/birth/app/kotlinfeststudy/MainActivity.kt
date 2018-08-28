f4a0e33ba3a3f62672c1c354c4eb2662772ab443
h3birth <h3.birth@gmail.com>
2018-08-28 15:06:45 +0900

    add study code "compareValuesBy"

--- app/src/main/java/h3/birth/app/kotlinfeststudy/MainActivity.kt
+++ app/src/main/java/h3/birth/app/kotlinfeststudy/MainActivity.kt
@@ -2,10 +2,25 @@ package h3.birth.app.kotlinfeststudy
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import h3.birth.app.kotlinfeststudy.session.A1AndroidQualityforKotlin
 
 class MainActivity : AppCompatActivity() {
+
+    private val study_session_index : Int = 1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        study( study_session_index )
+    }
+
+    fun study( study_session_index: Int ){
+        when( study_session_index ){
+            1 -> {
+                val A1AndroidQualityforKotlin = A1AndroidQualityforKotlin()
+                A1AndroidQualityforKotlin.studyDoCompareValueBy()
+            }
+        }
     }
 }
