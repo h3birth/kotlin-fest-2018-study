1f373d51b3b916a6f622ae0196d19ffbf0d583f0
h3birth <h3.birth@gmail.com>
2018-08-30 21:10:12 +0900

    add b4 session coroutine

--- app/src/main/java/h3/birth/app/kotlinfeststudy/MainActivity.kt
+++ app/src/main/java/h3/birth/app/kotlinfeststudy/MainActivity.kt
@@ -2,11 +2,17 @@ package h3.birth.app.kotlinfeststudy
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import h3.birth.app.kotlinfeststudy.dataclass.loadProfile
 import h3.birth.app.kotlinfeststudy.session.A1AndroidQualityforKotlin
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.experimental.android.UI
+import kotlinx.coroutines.experimental.async
+import kotlinx.coroutines.experimental.launch
 
 class MainActivity : AppCompatActivity() {
 
-    private val study_session_index: Int = 1
+    private val study_session_index: Int = 1 // or 4 セッションのコマを指定します
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,9 +23,17 @@ class MainActivity : AppCompatActivity() {
 
     fun study(study_session_index: Int) {
         when (study_session_index) {
-            1 -> {
+            1 -> { // A1セッション「Kotlin で改善する Android アプリの品質」のサンプル実行
                 val A1AndroidQualityforKotlin = A1AndroidQualityforKotlin()
                 A1AndroidQualityforKotlin.studyDoCompareValueBy()
+            }
+            4 -> { // B4 セッション「Kotlin コルーチンを理解しよう」のサンプル実行
+                launch(UI) {
+                    progressBar.visibility = View.VISIBLE
+                    val profile = async { loadProfile() }.await()
+                    textView.text = profile.name
+                    progressBar.visibility = View.GONE
+                }
             }
         }
     }
